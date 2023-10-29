4623907cb9fafe3c5231f0a8ffcbfe38d9578e55
DiegoBurbano352 <dburbanog1@miumg.edu.gt>
2023-10-29 15:33:59 -0600

    proyecto

---
+++ app/config/db.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+    HOST: "dpg-ckvbb2jamefc73erfsig-a.oregon-postgres.render.com",
+    USER: "proyecto_final_tgym_user",
+    PASSWORD: "zUa1VjSvnBB5WKuUglFa4Aetbdr2unXV",
+    DB: "proyecto_final_tgym",
+    dialect: "postgres",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
+  
